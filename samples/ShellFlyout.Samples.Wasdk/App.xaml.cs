9ef2f55f056f174411261c37921bb00be66d929e
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-10-16 19:31:43 +0900

    Move the tray icon logic to a class in the sample app

--- samples/ShellFlyout.Samples.Wasdk/App.xaml.cs
+++ samples/ShellFlyout.Samples.Wasdk/App.xaml.cs
@@ -2,18 +2,12 @@
 // Licensed under the MIT license.
 
 using Microsoft.UI.Xaml;
-using System;
 
 namespace U5BFA.ShellFlyout
 {
 	public partial class App : Application
 	{
-		public ShellFlyout? _trayIconFlyout;
-		public TrayIconMenuFlyout? _trayIconMenuFlyout;
-
 		private Window? _window;
-		private SystemTrayIcon? _systemTrayIcon;
-		private ContentBackdropManager? _backdropManager;
 
 		public App()
 		{
@@ -23,53 +17,15 @@ namespace U5BFA.ShellFlyout
 		protected override void OnLaunched(LaunchActivatedEventArgs args)
 		{
 			_window = new MainWindow();
-			_window.AppWindow.Hide();
+			_window.Activate();
 			_window.DispatcherQueue.EnsureSystemDispatcherQueue();
 
-			_systemTrayIcon = new SystemTrayIcon()
-			{
-				IconPath = "Assets\\TrayIcon.Dark.ico",
-				Tooltip = "Shell flyout sample app (WASDK)",
-				Id = new Guid("28DE460A-8BD6-4539-A406-5F685584FD4D")
-			};
-
-			_trayIconFlyout = new MainTrayIconFlyout();
-			_trayIconMenuFlyout = new MainTrayIconMeunFlyout();
-
-			_systemTrayIcon.Show();
-			_systemTrayIcon.LeftClicked += SystemTrayIcon_LeftClicked;
-			_systemTrayIcon.RightClicked += SystemTrayIcon_RightClicked;
-		}
-
-		private void SystemTrayIcon_LeftClicked(object? sender, EventArgs e)
-		{
-
-			if (_trayIconFlyout is null)
-				return;
-
-			if (_trayIconFlyout.IsOpen)
-				_trayIconFlyout.Hide();
-			else
-				_trayIconFlyout.Show();
-		}
-
-		private void SystemTrayIcon_RightClicked(object? sender, EventArgs e)
-		{
-			if (_trayIconMenuFlyout is null)
-				return;
-
-			if (_trayIconMenuFlyout.IsOpen)
-				_trayIconMenuFlyout.Hide();
-
-			_trayIconMenuFlyout.Show();
+			TrayIconManager.Default.Initialize();
 		}
 
 		~App()
 		{
-			_systemTrayIcon?.LeftClicked -= SystemTrayIcon_LeftClicked;
-			_systemTrayIcon?.Destroy();
-			_trayIconFlyout?.Dispose();
-			_backdropManager?.Dispose();
+			TrayIconManager.Default.Dispose();
 		}
 	}
 }
---
+++ samples/ShellFlyout.Samples.Wasdk/TrayIconManager.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 0x5BFA. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace U5BFA.ShellFlyout
+{
+	internal partial class TrayIconManager : IDisposable
+	{
+		private static readonly Lazy<TrayIconManager> _default = new(() => new TrayIconManager());
+		public static TrayIconManager Default => _default.Value;
+
+		private static SystemTrayIcon? _systemTrayIcon;
+		private static ContentBackdropManager? _backdropManager;
+		private static ShellFlyout? _trayIconFlyout;
+		private static TrayIconMenuFlyout? _trayIconMenuFlyout;
+
+		private TrayIconManager() { }
+
+		internal void Initialize()
+		{
+			_systemTrayIcon = new SystemTrayIcon()
+			{
+				IconPath = "Assets\\TrayIcon.Dark.ico",
+				Tooltip = "Shell flyout sample app (WASDK)",
+				Id = new Guid("28DE460A-8BD6-4539-A406-5F685584FD4D")
+			};
+
+			_trayIconFlyout = new MainTrayIconFlyout();
+			_trayIconMenuFlyout = new MainTrayIconMeunFlyout();
+
+			_systemTrayIcon.Show();
+			_systemTrayIcon.LeftClicked += SystemTrayIcon_LeftClicked;
+			_systemTrayIcon.RightClicked += SystemTrayIcon_RightClicked;
+		}
+
+		private void SystemTrayIcon_LeftClicked(object? sender, EventArgs e)
+		{
+			if (_trayIconFlyout is null)
+				return;
+
+			if (_trayIconFlyout.IsOpen)
+				_trayIconFlyout.Hide();
+			else
+				_trayIconFlyout.Show();
+		}
+
+		private void SystemTrayIcon_RightClicked(object? sender, EventArgs e)
+		{
+			if (_trayIconMenuFlyout is null)
+				return;
+
+			if (_trayIconMenuFlyout.IsOpen)
+				_trayIconMenuFlyout.Hide();
+
+			_trayIconMenuFlyout.Show();
+		}
+
+		public void Dispose()
+		{
+			_systemTrayIcon?.LeftClicked -= SystemTrayIcon_LeftClicked;
+			_systemTrayIcon?.RightClicked -= SystemTrayIcon_RightClicked;
+			_systemTrayIcon?.Destroy();
+			_trayIconFlyout?.Dispose();
+			_backdropManager?.Dispose();
+		}
+	}
+}
